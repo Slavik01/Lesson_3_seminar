480332f8ffc7b372536244e95bf2fae5112d7544
Slavik <slavik-skripka@yandex.ru>
2022-07-06 21:23:08 +0300

    1 version

---
+++ One/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// 1 вариант:
+
+// Console.WriteLine("Введите пятизначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine()); // вводим число и оно попадает в переменную number
+// string numberText = number.ToString(); // метод ToString => число переводит в строку и передает в переменную numberText
+// char[] namberMassiv =  numberText.ToCharArray(); // char[] => создаем массив char,что бы по одному символу считать нашу строку
+// // с помощью метода ToCharArray() => конвертируем текст из строкового формата в символьный массив.
+
+// Array.Reverse(namberMassiv); // метод Array.Reverse => переворачиват массив в обратном порядке.
+
+// string numberStrok = new string(namberMassiv); // обратно в строковый формат для сравнения
+
+// if (numberText == numberStrok)
+// {
+//     Console.Write("Да, это палиндром!");
+// }
+// else if (numberText != numberStrok)
+// {
+//     Console.Write("Не палиндром!");
+// }
+
+// 2 вариант 
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int a = number / 10000; 
+int b = ((number / 1000) % 10);
+int c = ((number / 100) % 10);
+int d = ((number / 10) % 10);
+int e = number % 10;
+
+if (a == e && b == d && c == c)
+{
+    Console.Write("Да, это палиндром!");
+}
+else
+{
+    Console.Write("Не палиндром!");
+}
---
+++ Two/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит
+//  расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Write("Введите координаты x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты x3: ");
+int x3 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты y3: ");
+int y3 = Convert.ToInt32(Console.ReadLine());
+// double distance = Math.Sqrt(Math.Pow(x1 - y1, 2) + Math.Pow(x2 - y2, 2) + Math.Pow(x3 - y3, 2));
+// Console.WriteLine($"Расстояние между тремя точками в пространстве равно: {distance}");
+
+// Метод:
+double distance (double x1, double y1,double x2, double y2,double x3, double y3) 
+{
+    return Math.Sqrt(Math.Pow(x1 - y1, 2) + Math.Pow(x2 - y2, 2) + Math.Pow(x3 - y3, 2));
+}
+Console.WriteLine($"Расстояние между тремя точками в пространстве равно: {Math.Round(distance(x1, y1, x2, y2, x3, y3))}");
+// double result = Console.WriteLine(distance (x1, y1, x2, y2, x3, y3));
+// Console.WriteLine($"Расстояние между тремя точками в пространстве равно: {Math.Round(result)}");
